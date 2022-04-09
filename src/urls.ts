6e9f9ace5219110e4fd909a6110de88bc7440ff5
Timo Nowak <Timo Nowak>
2022-04-10 01:41:02 +0200

    node out + https

--- src/urls.ts
+++ src/urls.ts
@@ -1,5 +1,5 @@
 export const FRONTEND_URL = 'http://localhost:3000';
-// export const FRONTEND_URL = 'http://vollsm.art';
+// export const FRONTEND_URL = 'https://vollsm.art';
 
 export const BACKEND_URL = 'http://localhost:3100';
-// export const BACKEND_URL = 'http://files.vollsm.art';
+// export const BACKEND_URL = 'https://files.vollsm.art';
